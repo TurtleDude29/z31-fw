a727da11c3e6997e263cc778ec055d91b9c30227
TurtleDude29 <yonatanatia29@gmail.com>
2024-08-04 17:36:16 -0400

    Update default_tune.cpp

--- default_tune.cpp
+++ default_tune.cpp
@@ -3,6 +3,6 @@
 void boardTuneDefaults() {
 	engineConfiguration->cylindersCount = 6;
 	engineConfiguration->displacement = 3.0;
-     engineConfiguration->firingOrder = FO_1_2_3_4_5_6;
+     	engineConfiguration->firingOrder = FO_1_2_3_4_5_6;
 	engineConfiguration->injector.flow = 260;
 }
