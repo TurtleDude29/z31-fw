4c46ea67b451edbf9327310c094bd749853178bb
TurtleDude29 <yonatanatia29@gmail.com>
2024-08-04 21:47:00 -0400

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -15,11 +15,6 @@ Gpio getWarningLedPin() {
 }
 
 void setBoardDefaultConfiguration() {
-	//TS SETTINGS
-	engineConfiguration->cylindersCount = 6;
-	engineConfiguration->displacement = 3.0;
-     	engineConfiguration->firingOrder = FO_1_2_3_4_5_6;
-	engineConfiguration->injector.flow = 260;
 	//MAIN
         engineConfiguration->mainRelayPin = Gpio::B8;
         engineConfiguration->fuelPumpPin = Gpio::E0;
