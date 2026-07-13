747b7638d5d4aa3fcf4a6213cf2a89b626b45f34
TurtleDude29 <yonatanatia29@gmail.com>
2026-07-13 17:32:45 -0400

    Update default_tune.cpp

--- default_tune.cpp
+++ default_tune.cpp
@@ -1,5 +1,8 @@
 #include "pch.h"
 
 void boardTuneDefaults() {
-    engineConfiguration->displacement = 1.65;
+    // Nissan VG30E
+    engineConfiguration->displacement = 3.00;
+    engineConfiguration->cylindersCount = 6;
+    engineConfiguration->firingOrder = FO_1_2_3_4_5_6;
 }
